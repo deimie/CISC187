f9cb36313d65170c179da0f83cf6353d0ddcf761
Evan <evanjoelsanchez@gmail.com>
2025-03-04 12:33:34 -0800

    Update main.cpp

--- assignments/assign5/main.cpp
+++ assignments/assign5/main.cpp
@@ -3,15 +3,24 @@
 using namespace std;
 
 int main() {
-    unordered_map<int, int> map =
-    {
-        {200,0},
-        {400,1},
-        {100,2},
-        {50,3},
-        {350,4}
-    };
     
-    auto val = map.find(400);
-    cout << val->second;
+    // TASK 1
+    // declare array
+    int arr[] = {200, 400, 100, 50, 350};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    // move array values into hash table
+    unordered_map<int, int> map;
+    for(int i = 0; i < size ; i++) {
+        map[arr[i]] = i; 
+    }
+
+    // search for a key and print value
+    auto val = map.find(50);
+    cout << "Found " << val->first << " at index " << val->second << endl;
+    
+    //TASK 2
+    string name = "Evan";
+    hash<string> h;
+    cout << "Name: " << name << "\nHash of name: " << h(name) << endl;
 }
