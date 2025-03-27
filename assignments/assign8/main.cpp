1336e0099fd81045544f1179fc295ff801c76ef0
Evan <evanjoelsanchez@gmail.com>
2025-03-26 17:52:53 -0700

    Create main.cpp

---
+++ assignments/assign8/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    
+    // constructor
+    Node(int inp) {
+        data = inp;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class BST {
+public:
+    Node* root = nullptr;
+    
+    void insert(int val) {
+        if(root == nullptr) {
+            root = new Node(val);
+        }
+        
+        Node* current = root;
+        while(true) {
+            if (val < current->data) {
+                if (current->left == nullptr) {
+                    current->left = new Node(val);
+                    break;
+                }
+                current = current->left;
+            } else {
+                if (current->right == nullptr) {
+                    current->right = new Node(val);
+                    break;
+                }
+                current = current->right;
+            }
+        }
+    }
+    
+};
+
+int main() {
+    BST tree;
+    int nums[] = {11, 4, 6, 8, 1, 4, 0, 3};
+    
+    for (int num: nums) {
+        tree.insert(num);
+    }
+}
